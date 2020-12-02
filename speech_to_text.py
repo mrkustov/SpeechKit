21beace53947caac5725bc491120ea96514d266d
mrkustov <kustov-am16@mail.ru>
2020-12-02 11:34:57 +0300

    This is the first version project

---
+++ speech_to_text.py
@@ -0,0 +1,105 @@
+import boto3
+import requests
+import time
+import json
+import os
+
+# Конфигурации
+
+
+# Регион (p. s. другой выбирать нельзя)
+REGION_NAME = 'ru-central1'
+# id-статического ключа доступа к Бакету
+AWS_SECRET_KEY_ID = 'YOUR__KEY_ID'
+# Секретный ключ (статического ключа доступа к Бакету)
+AWS_SECRET_ACCESS_KEY = 'YOUR__SECRET_ACCESS_KEY'
+# Имя Бакета
+bucket_name = 'you__backet_name'
+
+# API-ключ Сервисного аккаунта
+api_key = 'your_api_key_your_srv_acc'
+
+# Путь откуда берём аудиозаписи формата .ogg
+ogg_path = '/home/user_name/PycharmProjects/asr/ogg'
+# Путь куда кладём текст из результатов распознавания .txt
+txt_path = '/home/user_name/PycharmProjects/asr/text'
+
+
+def file_to_storage(file_path, bucket_name, result_file_name):
+    '''Функция возращает ссылку на аудиофайл из Бакета SpeechKit
+      предварительно загрузив его туда из локальной папки'''
+
+    session = boto3.session.Session(region_name=REGION_NAME, aws_access_key_id=AWS_SECRET_KEY_ID,
+                                    aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+
+    s3 = session.client(
+        service_name='s3',
+        endpoint_url='https://storage.yandexcloud.net'
+    )
+    link_head = "https://storage.yandexcloud.net/"
+
+    try:
+        s3.upload_file(file_path, bucket_name, result_file_name)
+        file_link = link_head + bucket_name + "/" + result_file_name
+    except:
+        file_link = None
+
+    return file_link
+
+
+def speech_to_text(api_key, filelink, result_file_name):
+    '''Функция распознования аудио и формирования текста аудио,
+    сохранив его в файл .txt в локальную папку'''
+
+    POST = "https://transcribe.api.cloud.yandex.net/speech/stt/v2/longRunningRecognize"
+
+    # Формируем сам текст запроса
+    body = {
+        "config": {
+            "specification": {
+                "languageCode": "ru-RU"
+            }
+        },
+        "audio": {
+            "uri": filelink
+        }
+    }
+
+    # Если вы хотите использовать IAM-токен для аутентификации, замените Api-Key на Bearer.
+    header = {'Authorization': 'Api-Key {}'.format(api_key)}
+
+    # Отправить запрос на распознавание.
+    req = requests.post(POST, headers=header, json=body)
+    data = req.json()
+    # print(data)
+
+    id = data['id']
+
+    # Запрашивать на сервере статус операции, пока распознавание не будет завершено.
+    while True:
+        time.sleep(1)
+
+        GET = "https://operation.api.cloud.yandex.net/operations/{id}"
+        req = requests.get(GET.format(id=id), headers=header)
+        req = req.json()
+
+        if req['done']: break
+        # print("Not ready")
+
+    # Сохранить текст из результатов распознавания в файл.
+    f = open(str(txt_path + '/' + result_file_name[:(len(result_file_name) - 4)] + '.txt'), 'w')
+    for chunk in req['response']['chunks']:
+        if chunk['channelTag'][0] == '1':
+            f.write(str(chunk['alternatives'][0]['text']) + '\n')
+    f.close()
+
+
+list_ogg = os.listdir(path=ogg_path)
+
+for audio in list_ogg:
+    print(audio)
+    file_path = str(ogg_path + '/' + audio)
+    result_file_name = str(audio[:(len(audio) - 4)] + '.ogg')
+    filelink = str(file_to_storage(file_path, bucket_name, result_file_name))
+    speech_to_text(api_key, filelink, result_file_name)
+print('Speech recognition completed')
---
+++ wav_to_ogg.py
@@ -0,0 +1,46 @@
+import os
+from pydub import AudioSegment
+
+# Конфигурации
+
+# Путь к аудиозаписям в формате .wav
+wav_path = '/home/username/PycharmProjects/asr/wav'
+
+# Путь куда кладём аудиозаписи формата .ogg
+ogg_path = '/home/username/PycharmProjects/asr/ogg'
+
+# Частота дискретизации
+ogg_sample_rate = 48000
+
+
+# a = os.listdir(path="/home/artem/PycharmProjects/asr/vrem")
+# print(a)
+
+
+
+def wav_to_ogg(wav_file_path, ogg_file_path, ogg_sample_rate, mono=False, left=True):
+    '''Функция коныертации аудио в формат .ogg
+    Так же можно выделять каждлого из спикеров при помощи "mono" и 'left'
+    '''
+    f = open(wav_file_path, 'r')
+    audio = AudioSegment.from_wav(f.name)
+    audio = audio.set_frame_rate(ogg_sample_rate)
+    if mono == True:
+        if left == True:
+            audio = audio.split_to_mono()[0]
+        else:
+            audio = audio.split_to_mono()[1]
+    print(audio)
+    audio.export(ogg_file_path, format="opus", bitrate='256k')
+    f.close()
+
+
+
+
+list_wav = os.listdir(path=wav_path)
+
+
+for audio in list_wav:
+    wav_file_path = str(wav_path + '/' + audio)
+    ogg_file_path = str(ogg_path + '/' + audio[:(len(audio)-4)] + '.ogg')
+    wav_to_ogg(wav_file_path, ogg_file_path, ogg_sample_rate)
